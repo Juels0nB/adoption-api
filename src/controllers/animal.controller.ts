1c526956d278664bd00d8478593406183ea2014f
Juelson Balanga <Juelsonbalanga@gmail.com>
2025-12-07 14:17:02 +0000

    adicionei codigo para aceitar page e limit

--- src/controllers/animal.controller.ts
+++ src/controllers/animal.controller.ts
@@ -2,42 +2,45 @@
 import { Request, Response } from 'express';
 import Animal from '../models/Animal';
 
-// Criar um novo animal (ou vários)
 export const createAnimal = async (req: Request, res: Response) => {
     try {
-        // Verifica se é um array (para inserires o teu JSON gigante de uma vez) ou um objeto só
         const data = req.body;
-
-        // Se for array, usa insertMany, se for objeto usa create
         const newAnimal = Array.isArray(data)
             ? await Animal.insertMany(data)
             : await Animal.create(data);
-
         res.status(201).json(newAnimal);
     } catch (error) {
         res.status(400).json({ message: 'Erro ao criar animal', error });
     }
 };
 
-// Buscar todos os animais (com filtros opcionais)
 export const getAnimals = async (req: Request, res: Response) => {
     try {
+        // 1. Lemos os parâmetros da Query (ex: ?page=2&limit=10)
+        // Se não vier nada, assumimos página 1 e limite 10
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const skip = (page - 1) * limit;
+
         const { species, status } = req.query;
         let filter: any = {};
 
-        // Se a App iOS pedir ?species=Cão, filtramos por isso
         if (species) filter.species = species;
         if (status) filter.status = status;
-        else filter.status = { $ne: 'Adotado' }; // Por defeito não mostra os adotados
+        else filter.status = { $ne: 'Adotado' };
 
-        const animals = await Animal.find(filter).sort({ createdAt: -1 }); // Mais recentes primeiro
+        // 2. Aplicamos o .skip() e .limit() na query do Mongoose
+        const animals = await Animal.find(filter)
+            .sort({ createdAt: -1 })
+            .skip(skip)   // Salta os anteriores
+            .limit(limit); // Traz só 10
+
         res.status(200).json(animals);
     } catch (error) {
         res.status(500).json({ message: 'Erro ao buscar animais', error });
     }
 };
 
-// Buscar um animal pelo ID (Para o ecrã de detalhes)
 export const getAnimalById = async (req: Request, res: Response) => {
     try {
         const animal = await Animal.findById(req.params.id);
